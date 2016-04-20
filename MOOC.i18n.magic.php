bf46c402fbd030cc2a62a35ac815496b17f3fa6b
Sebastian Schlicht <sebschlicht@uni-koblenz.de>
2016-04-20 11:34:22 +0200

    extension magic words file added

---
+++ MOOC.i18n.magic.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @since 1.0.0
+ *
+ * @file
+ *
+ * @licence GNU GPL
+ * @author Your Name (YourUserName)
+ */
+
+$magicWords = array();
+
+/** English
+ * @author Your Name (YourUserName)
+ */
+$magicWords['en'] = array(
+   'something' => array( 0, 'something' ),
+);
