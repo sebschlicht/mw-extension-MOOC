05a7fccc79b26e3340a53fd8dd243ca5b180bf18
Sebastian Schlicht <sebschlicht@uni-koblenz.de>
2016-09-20 19:41:40 +0200

    Revert "obsolete MOOC.* files removed"
    This reverts commit b68cf709ffa644bc186ca0bd9ec95779c4678d67.

---
+++ MOOC.i18n.magic.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @since 1.0.0
+ *
+ * @file
+ *
+ * @licence GNU GPL
+ * @author Your Name (YourUserName)
+ */
+
+$magicWords = array();
+
+/** English
+ * @author Your Name (YourUserName)
+ */
+$magicWords['en'] = array(
+   'MOOC' => array( 0, 'MOOC' ),
+);
