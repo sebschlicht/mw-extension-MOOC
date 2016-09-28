b9a09632d1569495b70e3d292737235efaf10dd7
Sebastian Schlicht <sebschlicht@uni-koblenz.de>
2016-09-28 16:50:08 +0200

    hook to set config variables, register new JS script

--- MOOC.hooks.php
+++ MOOC.hooks.php
@@ -6,8 +6,8 @@
  * @file
  * @ingroup Extensions
  *
- * @author Rene Pickhardt (User:renepick), Sebastian Schlicht (User:sebschlicht)
- * @license GPLv3
+ * @author Rene Pickhardt ([User:renepick]), Sebastian Schlicht (sebastian@jablab.de [User:sebschlicht])
+ * @license GPLv2
  */
 class MOOCHooks {
 
@@ -17,4 +17,19 @@ class MOOCHooks {
      * @param Parser $parser            
      */
     public static function onParserFirstCallInit(Parser &$parser) {}
+
+    /**
+     *
+     * @param array $vars            
+     */
+    public static function onResourceLoaderGetConfigVars(array &$vars) {
+        $vars['wgMOOC'] = [
+            'userAgentName' => 'MoocBot',
+            'userAgentUrl' => 'https://en.wikiversity.org/wiki/User:Sebschlicht',
+            'userAgentMailAddress' => 'sebschlicht@uni-koblenz.de',
+            'version' => '0.1'
+        ];
+        
+        return true;
+    }
 }
