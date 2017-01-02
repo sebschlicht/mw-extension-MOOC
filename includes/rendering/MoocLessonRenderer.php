4c17469816b668e7f5da40dcd150c18d2d7fe51f
Sebastian Schlicht <sebastian@jablab.de>
2017-01-02 17:20:09 +0100

    lesson: add child units to units section

--- includes/rendering/MoocLessonRenderer.php
+++ includes/rendering/MoocLessonRenderer.php
@@ -28,7 +28,8 @@ class MoocLessonRenderer extends MoocContentRenderer {
         if ($this->item->hasChildren()) {
             // list child units if any
             foreach ($this->item->children as $unit) {
-                $this->addChildItem($unit);
+                echo "adding child...";
+                $this->addChildUnit($unit);
             }
         } else {
             // show info box if no child units added yet
@@ -39,8 +40,56 @@ class MoocLessonRenderer extends MoocContentRenderer {
         $this->endSection();
     }
 
-    protected function addChildItem() {
-        // TODO implement
+    /**
+     * Adds an unit to the units section.
+     *
+     * @param MoocItem $unit unit to add
+     */
+    protected function addChildUnit($unit) {
+        $this->out->addHTML('<div class="child unit col-xs-12">');
+
+        // video thumbnail
+        $this->out->addHTML('<div class="video-thumbnail col-xs-12 col-sm-5">');
+        if (isset($unit->video)) {
+            // TODO re-calc max width
+            $this->out->addWikiText('[[File:' . $unit->video . '|frameless|300x170px|link=' . $unit->title . ']]');
+        } else {
+            // TODO make clickable without JS
+            $this->out->addHTML('<span>' . $this->loadMessage('units-no-video') . '</span>');
+        }
+        $this->out->addHTML('</div>');
+        $this->parserOutput->addLink($unit->title);
+
+        $this->out->addHTML('<div class="col-xs-12 col-sm-7">');
+        $this->out->addHTML('<div class="title">');
+        $this->out->addWikiText('[[' . $unit->title . '|'. $unit->title->getSubpageText() . ']]');
+        $this->out->addHTML('</div>');
+
+        // links TODO
+
+        // learning goals
+        $learningGoals = $this->generateLearningGoalsWikiText($unit);
+        if ($learningGoals != null) {
+            $this->out->addWikiText($learningGoals);
+        } else {
+            $this->out->addHTML($this->loadMessage('section-' . 'learning-goals' . '-empty-description'));
+        }
+
+        // meta TODO
+
+        $this->out->addHTML('</div>');
+        $this->out->addHTML('</div>');
+    }
+
+    protected function addSectionActions($sectionKey, $sectionName) {
+        switch ($sectionKey) {
+            // TODO make this a const somewhere
+            case 'units':
+                // TODO add button "Add Unit"
+
+            default:
+                parent::addSectionActions($sectionKey, $sectionName);
+        }
     }
 
     protected function getSectionIconFilename($sectionKey) {
