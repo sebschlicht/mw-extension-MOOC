5682e354eb4ac3dac27765d2aaedc6881b5a1036
Sebastian Schlicht <sebastian@jablab.de>
2017-01-04 18:34:52 +0100

    stub to add units added

--- resources/js/ext.mooc.edit.js
+++ resources/js/ext.mooc.edit.js
@@ -13,9 +13,48 @@
   // fill modal boxes with item content
   fillModalBoxes( item );
   
-  //mw.loader.using( 'mediawiki.api', loadItem, function() {
-  //  console.error( 'failed to load mw.Api' );
-  //} );
+  mw.loader.using( 'mediawiki.api.edit', registerApiCalls, function() {
+    console.error( 'failed to load mw.Api' );
+  } );
+
+  function registerApiCalls() {
+    $( '#units.section .header form.add .btn-submit' ).on('click', function() {
+      var $btn = $(this);
+      var $form = $btn.parent('form');
+      var $value = $form.find('.value');
+      // TODO validate
+      var $name = $value.val();
+      var title = mw.config.get('wgPageName') + '/' + $name;
+      console.log('creating ' + title);
+
+      var $api = new mw.Api();
+      $api.edit(title, {
+        action: 'edit',
+        createonly: true,
+        summary: 'Adding the lesson "' + $name + '".',
+        text: '{"type":"unit"}',
+        // TODO badformat why?
+        //contentformat: 'application/json',
+        // TODO currently not possible when logged out (or even non-admin?)
+        contentmodel: 'mooc-item'
+      }).then(function (json) {
+        // TODO setup mw loggingg
+        console.log(json);
+      }).fail(function (code, result) {
+        if (code === "http") {
+          console.log("HTTP error: " + result.textStatus); // result.xhr contains the jqXHR object
+          console.log(result);
+        } else if (code === "ok-but-empty") {
+          console.log("Got an empty response from the server");
+        } else {
+          console.log("API error: " + code);
+          console.log(result);
+        }
+      });
+
+      return false;
+    });
+  }
   
   // TODO: if possible, we should load the VisualEditor instead
   function fillModalBoxes( item ) {
@@ -35,7 +74,9 @@
   function fillModalBox( id, content ) {
     var $input = $('#mooc #' + id + '.section .header form.edit .value');
     $input.val(content);
-    if ($input.is('textarea')) {
+
+    // register resizing for textareas
+    if ($input.is('textarea.auto-grow')) {
         resizeTextarea($input);
         $input.on('keyup', textareaValueChanged);
     }
