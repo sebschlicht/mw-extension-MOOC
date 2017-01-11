f50a6fed129ac2d0e68081d15efb9bf9eb06a7d7
Sebastian Schlicht <sebastian@jablab.de>
2017-01-11 16:51:48 +0100

    more efficient MOOC structure loading auto-detecting MOOC item subpages

--- includes/structure/MoocContentStructureProvider.php
+++ includes/structure/MoocContentStructureProvider.php
@@ -16,91 +16,81 @@ class MoocContentStructureProvider {
      *
      * @param MoocItem $item MOOC item that the structure is needed for
      */
-    public static function loadMoocStructure(&$item) {
-        $rootTitle = $item->title->getRootTitle();
-        // TODO use getSubpages (once working) to fetch subpages and build a query to get their content?
-        // TODO if not working (e.g delayed) fetch all pages LIKE title/*, filter children and query their content
-        $baseItem = self::loadMoocStructureFromTitle($rootTitle);
-        $item->baseItem = $baseItem;
+    public static function loadMoocStructure(&$renderedItem) {
+        // load all MOOC items from database
+        $rootPageTitle = $renderedItem->title->getRootTitle()->getDBkey();
+        //TODO we know the ID pretty well
+        $namespace = $renderedItem->title->getNamespace();
 
-        $queue = [ $baseItem ];
-        while (!empty($queue)) {
-            $crr = array_pop($queue); // LIFO until reversing array
-
-            // check if item is what we are searching for
-            if ($item->title->equals($crr->title)) {
-                $item->children = $crr->children;
-                return;
-            }
-            // continue with its children
-            if ($crr->hasChildren()) {
-                foreach ($crr->children as $child) {
-                    array_push($queue, $child);
-                }
-            }
-        }
-        /*
-         * approaches:
-         * 1. single db-query to identifty sub-titles with appropriate content model
-         * 2. use explicit child items that are sub-titles a) either using a single db-query or b) recursive queries
-         *
-         * currently: 2b (worst performance)
-         * TODO use what is most reasonable
-         */
-    }
-
-    /**
-     * Loads the whole structure of a MOOC item (all its children down to the units).
-     *
-     * @param Title $title title of the MOOC item
-     * @return MoocItem MOOC item
-     */
-    private static function loadMoocStructureFromTitle(&$title) {
-        // load single page from DB
-        $text = MoocContentStructureProvider::loadPageText($title);
-        // load MoocItem from page content (JSON)
-        $contentModel = new MoocContent($text);
-        $item = $contentModel->loadItem();
-        $item->setTitle($title);
-
-        // recursively load children via title
-        $children = [];
-        foreach ($item->childNames as $childName) {
-            $childTitle = Title::newFromText($title . '/' . $childName);
-            $childStructure = MoocContentStructureProvider::loadMoocStructureFromTitle($childTitle);
-            array_push($children, $childStructure);
-        }
-        $item->children = $children;
-        return $item;
-    }
-
-    /**
-     * Loads the content of a single page from the database.
-     *
-     * @param Title $title page title
-     * @return string page content loaded from the database
-     */
-    private static function loadPageText($title) {
         $db = wfGetDB(DB_SLAVE);
-        // TODO try this neat (cached) function
-        //$rev = Revision::loadFromTitle($db, $title);
-        //return $rev->getContent()->getNativeData();
-        $row = $db->select(array(
-            'text',
-            'revision'
-        ), array(
-            'old_text'
-        ), array(
-            'rev_id' => $title->getLatestRevID()
-        ), __METHOD__, array(), 
-            array(
-                'revision' => array(
+        $res = $db->select( array(
+                'text',
+                'revision',
+                'page'
+            ), array(
+                'page_title',
+                'old_text'
+            ), "page_title LIKE '$rootPageTitle%' AND page_namespace = '$namespace'",
+            __METHOD__, array(
+                'ORDER_BY' => 'page_title ASC'
+            ), array(
+                'page' => array(
+                    'INNER JOIN',
+                    array(
+                        'rev_id=page_latest'
+                    )
+                ), 'revision' => array(
                     'INNER JOIN',
                     array(
                         'old_id=rev_text_id'
                     )
                 )
             ));
-        return $row->current()->old_text;
+
+        $items = [];
+        foreach ( $res as $row ) {
+            $contentModel = new MoocContent( $row->old_text );
+            $item = $contentModel->loadItem();
+            $item->setTitle( Title::newFromText( $row->page_title, $namespace ) );
+            array_push( $items, $item );
+        }
+
+
+        // load structure from item titles
+        $rootItem = array_shift( $items );
+        $prevLesson = null;
+        foreach ( $items as $item ) {
+            // determine item parent
+            $parent = $rootItem;
+            if ($prevLesson != null && $item->title->isSubpageOf( $prevLesson->title )) {
+                // child unit of previous lesson
+                $parent = $prevLesson;
+            } else {
+                // lesson
+                $prevLesson = $item;
+            }
+
+            // register item as child of parent
+            if ( !isset( $parent->children ) ) {
+                $parent->children = [];
+            }
+            array_push( $parent->children, $item );
+        }
+        $renderedItem->baseItem = $rootItem;
+
+        // inject values of the rendered item
+        $queue = [$rootItem];
+        while ( !empty($queue) ) {
+            $crr = array_pop( $queue ); // LIFO until reversing array
+            // check if item is what we are searching for
+            if ( $renderedItem->title->equals( $crr->title ) ) {
+                $renderedItem->children = $crr->children;
+                return;
+            } elseif ($crr->hasChildren()) {
+                foreach ($crr->children as $child) {
+                    array_push($queue, $child);
+                }
+            }
+        }
     }
 }
